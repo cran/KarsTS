b6a4ac0efaf29c6b30713e98d9ed89d10d696ff9
Marina Saez <marinasaez_andreu@hotmail.com>
2017-06-02 04:36:55 +0000

    version 1.1

--- R/getScreenSize.R
+++ R/getScreenSize.R
@@ -1,5 +1,4 @@
-getScreenSize <-
-function() {
+getScreenSize <- function() {
   
   opeSys <- Sys.info()[[1]]
   
@@ -30,45 +29,11 @@ function() {
       
     }
     
-    
   }else{
     
-    screenSizeMac <- try(system(paste0("system_profiler ",
-                                       "SPDisplaysDataType |grep Resolution"), 
-                                intern = TRUE), silent = TRUE)
-    
-    if (class(screenSizeMac) != "try-error") {
-      
-      sizeToNumeric <- sapply(strsplit(screenSizeMac, split = NULL), 
-                              as.numeric)
-      reallyNumeric <- which(is.finite(sizeToNumeric))
-      trueCipher <- sizeToNumeric[min(reallyNumeric):max(reallyNumeric)]
-      separation <- which(is.na(trueCipher))
-      screenWidthMac <- trueCipher[1:(min(separation) - 1)]
-      screenHeightMac <- trueCipher[(max(separation) + 1):length(trueCipher)]
-      concatenate <- function(X) {
-        res <- NULL
-        for (i in 1:length(X)) {
-          res <- paste0(res, X[i])
-        }
-        res
-      }
-      screenWidthMac <- as.numeric(concatenate(as.character(screenWidthMac)))
-      screenHeightMac <- as.numeric(concatenate(as.character(screenHeightMac)))
-      screenSize <- c(screenWidthMac, screenHeightMac)
-      
-      
-      screenSize <- round((2/3) * screenSize)
-      names(screenSize) <- c("width", "height")
-      assign("screenSize", screenSize, envir = KTSEnv)
-      
-    }else{
-      
-      screenSize <- c(900, 500)
-      names(screenSize) <- c("width", "height")
-      assign("screenSize", screenSize, envir = KTSEnv)
-      
-    }
+    screenSize <- c(900, 500)
+    names(screenSize) <- c("width", "height")
+    assign("screenSize", screenSize, envir = KTSEnv)
     
   }
   
